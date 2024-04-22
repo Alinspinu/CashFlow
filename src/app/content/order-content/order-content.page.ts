907d9069bcabd64a6f2b354920d657bac641ad59
Alin Spinu <alinspinu@Alins-MacBook-Pro.local>
2024-04-22 08:59:43 +0300

    all done

--- src/app/content/order-content/order-content.page.ts
+++ src/app/content/order-content/order-content.page.ts
@@ -4,13 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 import { BillPage } from './bill/bill.page';
 import { MeniuPage } from './meniu/meniu.page';
-// import { ActivatedRoute } from '@angular/router';
-// import { Subscription } from 'rxjs';
-// import { Bill, BillProduct, Table } from 'src/app/models/table.model';
-// import { emptyBill, emptyTable } from 'src/app/models/empty-models';
-// import { TablesService } from 'src/app/tables/tables.service';
-// import { WebRTCService } from '../webRTC.service';
-// import { round } from '../../../../../../CashFlow/src/app/shared/utils/functions';
+
 
 
 
